e9c0808cd388fe644d315ff6bd961225cafe4cfc
Annie Tsai <annietsai@berkeley.edu>
2016-06-29 17:07:25 -0700

    debug and style check

--- web_scraper.py
+++ web_scraper.py
@@ -6,76 +6,94 @@ import sys
 import re
 
 URL = 'http://www.yelp.com'
+SEARCH = '/search?'
+START = 'start'
+START_SEARCH_AT = '&start='
 
 def proceed(counter):
     """Determines whether or not a search query continues. COUNTER determines
     which question to prompt the user with.
     """
     if counter == 0:
-        cont = input('There seem to be no matches to your request. '
-            + 'Try again? Enter [y/n].\n>> ')
+        try_again = input('There seem to be no matches to your request. '
+                        + 'Try again? Enter [y/n].\n>> ')
     else:
-        cont = input('Please enter [y/n].\n>> ')
+        try_again = input('Please enter [y/n].\n>> ')
 
-    if cont == 'y':
+    if try_again == 'y':
         return None
-    elif cont == 'n':
-        restart = 'q'
+    elif try_again == 'n':
+        i = 0
+        restart = None
         while restart != 'y' and restart != 'n':
-            restart = input('Would you like to restart your search?\n>> ')
+            if i == 0:
+                restart = input('Would you like to restart your search?\n>> ')
+            else:
+                restart = input('Please enter [y/n].\n>> ')
+
             if restart == 'y':
-                return 'start'
+                return START
             elif restart == 'n':
                 sys.exit()
             else:
-                print('Please enter [y/n].')
+                i += 1
     else:
         return proceed(counter + 1)
 
 def startquit(string):
     """Checks if the user inputted a STRING to restart the search or quit."""
     if string == 'start' or string == '-s':
-        return 'start'
+        return START
     elif string == 'quit' or string == '-q':
         sys.exit()
     return None
 
-def result_soup(url, query):
-    """Takes in a string URL and turns it into soup, which is then dissected
-    to find particular strings. The QUERY determines which path to take.
+def main_content(url):
+    """Takes in a URL and finds the path to the main content of the webpage
+    corresponding that URL.
     """
     page = requests.get(url).content
     soup = BeautifulSoup(page, 'html.parser')
-    main_content = soup.body.select('#wrap')[0].select('.main-content-wrap--full')[0]
+    return soup.body.select('#wrap')[0].select('.main-content-wrap--full')[0]
+
+def result_soup(url, query):
+    """Takes in a string URL and returns text associated with whether or not the
+    webpage corresponding to that url has any results. The QUERY determines which
+    path to take.
+    """
+    content = main_content(url)
     if query == 'location':
         try:
-            result = main_content.select('#super-container')[0]\
-                .select('.container')[0].select('.search-exception')[0]\
-                .select('.column-alpha')[0].select('.content')[0].h2.getText()
+            result = content.select('#super-container')[0]\
+                                 .select('.container')[0]\
+                                 .select('.search-exception')[0]\
+                                 .select('.column-alpha')[0]\
+                                 .select('.content')[0].h2.getText()
         except:
             result = 'ok'
     else:
-        result = main_content.select('.top-shelf-grey')[0]\
-            .select('.content-container')[0].select('.search-page-top')[0]\
-            .select('.column-alpha')[0].select('.clearfix')[0].h1.getText()
+        result = content.select('.top-shelf-grey')[0]\
+                             .select('.content-container')[0]\
+                             .select('.search-page-top')[0]\
+                             .select('.column-alpha')[0]\
+                             .select('.clearfix')[0].h1.getText()
     return result
 
 def quality_f(url, quality, reviews, maximum, search, attrs):
-    """Looks at URL to find all results matching QUALITY and REVIEWS. Returns the
-    MAXIMUM number of results or less. SEARCH provides the url text before 'start?'
-    and ATTRS provifes the url text after 'start?'.
+    """Looks at the webpage at URL to find all results matching the user's requested
+    QUALITY and REVIEWS. Returns the MAXIMUM number of results or less. SEARCH provides
+    the url string before '/start?' and ATTRS provifes the url string after '/start?'.
     """
     results = []
     i = 0
-    while len(results) < int(maximum):
-        page = requests.get(url).content
-        soup = BeautifulSoup(page, 'html.parser')
-        main_content = soup.body.select('#wrap')[0].select('.main-content-wrap--full')[0]
-
-        businesses = main_content.select('#super-container')[0]\
-            .select('.container')[0].select('.search-results-block')[0]\
-            .select('.column-alpha')[0].select('.indexed-biz-archive')[0]\
-            .select('.search-results-content')[0]
+    while len(results) < maximum:
+        content = main_content(url)
+        businesses = content.select('#super-container')[0]\
+                                 .select('.container')[0]\
+                                 .select('.search-results-block')[0]\
+                                 .select('.column-alpha')[0]\
+                                 .select('.indexed-biz-archive')[0]\
+                                 .select('.search-results-content')[0]
         try:
             businesses = businesses.select('.no-results')[0]
             break
@@ -84,29 +102,34 @@ def quality_f(url, quality, reviews, maximum, search, attrs):
 
         for biz in businesses:
             biz_info = biz.select('.natural-search-result')[0]\
-                .select('.biz-listing-large')[0].select('.main-attributes')[0]\
-                .select('.media-block--12')[0].select('.media-story')[0]
+                          .select('.biz-listing-large')[0]\
+                          .select('.main-attributes')[0]\
+                          .select('.media-block--12')[0]\
+                          .select('.media-story')[0]
 
             try:
                 rating = biz_info.select('.biz-rating')[0].select('.rating-large')[0].i
-                stars = str(rating).split(' ')[2].split('_')[1].strip('"')
-                if int(quality) <= int(stars):
-                    biz_rating = biz_info.select('.biz-rating')[0].span.getText()
-                    rating_num = biz_rating.split(' ')
-                    rating_num[:] = (value for value in rating_num if value != '')
+                stars = int(str(rating).split(' ')[2].split('_')[1].strip('"'))
+                if quality <= stars:
+                    biz_rating_text_lst = biz_info.select('.biz-rating')[0]\
+                                                  .span.getText().split(' ')
+                    biz_rating_text_lst[:] = (
+                        value for value in biz_rating_text_lst if value != ''
+                        )
+                    biz_rating = int(biz_rating_text_lst[1])
 
-                    if int(rating_num[1]) >= int(reviews):
+                    if biz_rating >= reviews:
                         link = URL + biz_info.h3.span.select('a[href]')[0]['href']
                         results.append(link)
             except:
                 pass
 
         i += 10
-        url = URL + search + '&start=' + str(i) + attrs
+        url = URL + search + START_SEARCH_AT + str(i) + attrs
 
-    bound = max(len(results), int(maximum))
-    if bound > int(maximum):
-        bound = int(maximum)
+    bound = max(len(results), maximum)
+    if bound > maximum:
+        bound = maximum
     return results[:bound]
 
 def main():
@@ -131,14 +154,14 @@ def main():
             print('There are multiple locations matching your search. Please try')
             location = input('inputting [city, state] or [city, country].\n>> ')
 
-        if startquit(location) == 'start':
+        if startquit(location) == START:
             i = 0
             location = None
             continue
 
         location = re.sub('\s+', '+', location)
         loc_url = 'find_loc=' + location
-        new_url = URL + '/search?' + loc_url
+        new_url = URL + SEARCH + loc_url
         result = result_soup(new_url, 'location')
 
         if 'Sorry' in result:
@@ -154,20 +177,20 @@ def main():
         while search is None:
             search = input('What are you searching for?\n>> ')
 
-            if startquit(search) == 'start':
-                search = 'start'
+            if startquit(search) == START:
+                search = START
                 break
 
             search = re.sub('\s+', '+', search)
             search_url = 'find_desc=' + search
-            new_url = URL + '/search?' + search_url + '&' + loc_url
+            new_url = URL + SEARCH + search_url + '&' + loc_url
             result = result_soup(new_url, 'search')
 
             if 'No Results' not in result:
                 break
             search = proceed(0)
 
-        if search == 'start':
+        if search == START:
             i = 0
             location = None
             continue
@@ -179,26 +202,31 @@ def main():
                 print('What is your price range? Please enter input in the format')
                 price = input('"[lower] to [upper]" or "[lower]-[upper]".\n>> ')
             else:
-                new_url = URL + '/search?' + search_url + '&' + loc_url
+                new_url = URL + SEARCH + search_url + '&' + loc_url
                 price = input('There seems to be an error; please enter a valid price '
                     + 'range.\n>> ')
 
-            if startquit(price) == 'start':
-                price = 'start'
+            if startquit(price) == START:
+                price = START
                 break
 
-            price_lst = []
+            price_text_lst = []
             if 'to' in price:
-                price_lst = price.split(' ')
-                price_lst.remove('to')
+                price_text_lst = price.split(' ')
+                price_text_lst.remove('to')
             elif '-' in price:
-                price_lst = price.split('-')
-            price_lst[:] = (value for value in price_lst if value != '')
-            price_lst = ([p.replace('$', '') for p in price_lst])
+                if price.startswith('-'):
+                    j += 1
+                    price = None
+                    continue
+                price_text_lst = price.split('-')
+
+            price_text_lst = ([value for value in price_text_lst if value != ''])
+            price_text_lst = ([p.replace('$', '') for p in price_text_lst])
 
             try:
-                lower_bound = int(price_lst[0])
-                upper_bound = int(price_lst[1])
+                lower_bound = int(price_text_lst[0])
+                upper_bound = int(price_text_lst[1])
                 if lower_bound < 0 or lower_bound > upper_bound:
                     j += 1
                     price = None
@@ -212,7 +240,7 @@ def main():
                     price_url = price_url + 'RestaurantsPriceRange2.3,'
                 if upper_bound > 60:
                     price_url = price_url + 'RestaurantsPriceRange2.4,'
-                new_url = new_url + '&start=0' + price_url
+                new_url = new_url + START_SEARCH_AT + '0' + price_url
             except:
                 j += 1
                 price = None
@@ -225,7 +253,7 @@ def main():
                 if price is None:
                     j = 0
 
-        if price == 'start':
+        if price == START:
             i = 0
             location = None
             continue
@@ -240,8 +268,8 @@ def main():
                 num_results = input('There seems to be an error; please enter a valid '
                     + 'number.\n>> ')
 
-            if startquit(num_results) == 'start':
-                num_results = 'start'
+            if startquit(num_results) == START:
+                num_results = START
                 break
 
             try:
@@ -249,9 +277,8 @@ def main():
             except:
                 num_results = None
                 k += 1
-                continue
 
-        if num_results == 'start':
+        if num_results == START:
             i = 0
             location = None
             continue
@@ -266,8 +293,8 @@ def main():
                 quality = input('There seems to be an error; please enter a number '
                     + '1 through 4.\n>> ')
 
-            if startquit(quality) == 'start':
-                quality = 'start'
+            if startquit(quality) == START:
+                quality = START
                 break
 
             try:
@@ -280,7 +307,7 @@ def main():
                 quality = None
                 continue
 
-        if quality == 'start':
+        if quality == START:
             i = 0
             location = None
             continue
@@ -295,8 +322,8 @@ def main():
                 reviews = input('There seems to be an error; please enter a valid '
                     + 'number.\n>> ')
 
-            if startquit(reviews) == 'start':
-                reviews = 'start'
+            if startquit(reviews) == START:
+                reviews = START
                 break
 
             try:
@@ -309,15 +336,15 @@ def main():
                 reviews = None
                 continue
 
-            search_location_url = '/search?' + search_url + '&' + loc_url
-            result_lst = quality_f(new_url, quality, reviews, num_results,
+            search_location_url = SEARCH + search_url + '&' + loc_url
+            result_lst = quality_f(new_url, int(quality), int(reviews), int(num_results),
                 search_location_url, price_url)
             if len(result_lst) == 0:
                 reviews = proceed(0)
                 if reviews is None:
                     m = 0
 
-        if reviews == 'start':
+        if reviews == START:
             i = 0
             location = None
             continue
